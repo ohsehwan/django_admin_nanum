242bb3b08d56f85cb10011833a180f8255d6c4d9
Seong Gi Dong <dirrksdl@naver.com>
2019-05-02 09:10:52 +0900

    팀리더 조회

--- service20/views.py
+++ service20/views.py
@@ -7232,6 +7232,9 @@ class MP0101M_adm_list(generics.ListAPIView):
         query += " and std_detl_code = C.status)) as status_nm,  "
         query += " (select indv_div from service20_mpgm where mp_id = A.mp_id) indv_div, "
 
+        # 팀 리더
+        query += " (select ldr_id from service20_mp_team where mp_id = A.mp_id and team_id = A.team_id) ldr_id, "
+
         query += " C.mp_name,B.pr_yr,B.pr_sch_yr,B.pr_term_div,A.* from service20_mp_mtr A left join service20_vw_nanum_stdt B on (A.apl_id = B.apl_id),service20_mpgm C where A.mp_id = C.mp_id and A.mp_id = '"+mp_ida+"' and A.apl_id='"+ida+"'"
         queryset = mp_mtr.objects.raw(query)
         print(query)
